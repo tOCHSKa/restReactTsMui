acfba1d2b1ae03fee4507460b69168504f08866c
tOCHSKa <144660217+tOCHSKa@users.noreply.github.com>
2024-07-15 11:31:02 +0200

    Accueil 1.0

--- Front-ReactViteTsMUI/vite-project/src/components/Accueil.tsx
+++ Front-ReactViteTsMUI/vite-project/src/components/Accueil.tsx
@@ -1,11 +1,29 @@
+import { Box, styled, Typography } from '@mui/material';
 import React from 'react';
+import {theme} from "./theme"
+
+
+const Image = styled('img')({
+    height: '74vh',
+    width: '100%'
+});
 
 const Accueil: React.FC = () => {
     return (
-        <div>
-            <h1>Accueil</h1>
-            <p>Bienvenue sur la page d'accueil.</p>
-        </div>
+            <Box sx={{ backgroundColor: theme.palette.secondary.main,
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+             }}>
+                <Image src="/herobanner.png" alt="Accueil" />
+                <Typography variant='h6' sx={{paddingTop: theme.spacing(5)}}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi non.
+                </Typography>
+                <Typography variant='h2' pb={5} sx={{ fontFamily: 'Italianno, cursive',paddingBottom: theme.spacing(5)}}>
+                    This is Italianno Font
+                </Typography>
+            </Box>
     );
 };
 
